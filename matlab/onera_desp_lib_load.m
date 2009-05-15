69a77d9176b5ca1d28eb3f27509ac5a546d9c38b
Paul O'Brien <tpoiii@irbem.sf>
2009-05-15 12:47:25 +0000

    made changes to get Matlab to work right on 32 or 64 bit systems
    git-svn-id: svn://svn.code.sf.net/p/irbem/code/trunk@79 80555d2d-b7e3-4ae1-aded-73e86dfc2014

--- matlab/onera_desp_lib_load.m
+++ matlab/onera_desp_lib_load.m
@@ -22,19 +22,26 @@ function onera_desp_lib_load(libfile,headerfile)
 %function onera_desp_lib_load(libfile,@prototypefunction);
 %function onera_desp_lib_load(libfile,headerfile);
 % checks for the presence of the onera_desp_lib dynamic library in memory
-% if not present, attempts to load it using either a prototype function
-% or a headerfile
+% if not present, attempts to load it using a headerfile
 if ~libisloaded('onera_desp_lib'),
     if nargin < 2,
-        if exist('onera_desp_lib_proto.m','file'),
-            headerfile = @onera_desp_lib_proto; % use prototype file
-        else
-            headerfile = 'onera_desp_lib'; % use headerfile
-        end
+       if ~isempty(findstr(computer,'64')), % 64-bit architecture
+           headerfile = 'onera_desp_lib_64.h';
+       else,
+           headerfile = 'onera_desp_lib.h';
+       end
     end
     if nargin < 1,
-        libfile = 'onera_desp_lib';
+        if ispc,
+           libfile = 'onera_desp_lib.dll';
+        elseif ismac,
+           libfile = 'onera_desp_lib.dylib';
+        else,
+           libfile = 'onera_desp_lib.so';
+        end
     end
     loadlibrary(libfile,headerfile,'alias','onera_desp_lib');
 end
+
+% command to generate proto file -- don't do this anymore as it creates problems for 64-bit machines
 % loadlibrary('onera_desp_lib.dll','onera_desp_lib.h','alias','onera_desp_lib','mfilename','onera_desp_lib_proto.m'); disp('remember to move the proto file');
