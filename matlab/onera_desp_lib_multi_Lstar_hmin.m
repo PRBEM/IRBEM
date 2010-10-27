9ac5abff7f82fe989b48d95ea9cd1ccac1b13c00
Paul O'Brien <tpoiii@irbem.sf>
2010-10-27 14:46:56 +0000

    minor mods: flags, magic word, maginput_varies
    git-svn-id: svn://svn.code.sf.net/p/irbem/code/trunk@239 80555d2d-b7e3-4ae1-aded-73e86dfc2014

--- matlab/onera_desp_lib_multi_Lstar_hmin.m
+++ matlab/onera_desp_lib_multi_Lstar_hmin.m
@@ -31,10 +31,28 @@ if isempty(maginput),
     maginput = zeros(25,1);
 end
 
+if numel(maginput)==25,
+    maginput = reshape(maginput,1,25); % row vector
+end
+
+if size(maginput,2) ~= 25,
+    error('maginput is wrong size. dim 2 should be 25 elements');
+end
+
 ntimes = numel(dates);
 
 if numel(alpha)==1,
-    alpha = repmat(ntimes,1);
+    alpha = repmat(alpha,ntimes,1);
+end
+
+magic = 'multi_Lstar_hmin';
+header = char(zeros(100,1)); % 100-byte header
+header(1:length(magic)) = magic; % magic string
+
+% control for maginput varying
+maginput_varies = numel(maginput)>25;
+if maginput_varies,
+    header(length(magic)+1) = char(1); % maginput varies with time
 end
 
 kext = onera_desp_lib_kext(kext);
@@ -42,12 +60,13 @@ options = onera_desp_lib_options(options);
 sysaxes = onera_desp_lib_sysaxes(sysaxes);
 [iyear,idoy,UT] = onera_desp_lib_matlabd2yds(dates);
 fid = fopen(filename,'wb');
+fwrite(fid,header,'char');
 fwrite(fid,ntimes,'int32');
 fwrite(fid,kext,'int32');
 fwrite(fid,options,'int32');
 fwrite(fid,sysaxes,'int32');
-fwrite(fid,maginput,'double');
 fwrite(fid,R0,'double');
+fwrite(fid,maginput','double');
 fwrite(fid,iyear,'int32');
 fwrite(fid,idoy,'int32');
 fwrite(fid,UT,'double');
@@ -68,3 +87,14 @@ I = fread(fid,ntimes,'double');
 hmin = fread(fid,ntimes,'double');
 hmin_lon = fread(fid,ntimes,'double');
 fclose(fid);
+
+% flag is -1.0000e+031
+
+Lm(Lm < -1e+30) = nan;
+Lstar(Lstar < -1e+30) = nan;
+Bmin(Bmin < -1e+30) = nan;
+Bmir(Bmir < -1e+30) = nan;
+I(I < -1e+30) = nan;
+hmin(hmin < -1e+30) = nan;
+hmin_lon(hmin_lon < -1e+30) = nan;
+
